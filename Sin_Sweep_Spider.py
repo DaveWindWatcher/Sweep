b32261dd3f091cd92331d3e138ba53181cdcb322
DaveWindWatcher <davewindwatcher@gmail.com>
2022-07-11 13:25:27 +0200

    'sweep_python_1st_version'

---
+++ Sin_Sweep_Spider.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jul 11 12:25:42 2022
+
+@author: DaveW
+
+Implementation of a n order high-pass with w0 threshold frequency filter to a sin function
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+# The sin function will be tha one that provides numpy
+
+#%% Creation of the frequency list
+
+Frequency = np.linspace(0,20000, 1e6)
+print(Frequency)
+
+
+
+
+
+
+
+
+
+
+
